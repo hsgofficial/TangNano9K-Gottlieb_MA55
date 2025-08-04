0e42c7685bc3b4a9a65a25cf5cc24e143f88b680
Pinball Wiz <70185819+pinballwizz@users.noreply.github.com>
2025-08-04 15:38:39 +1000

    Add files via upload

---
+++ TN9K-Gottlieb_MA55/tools/make_sound_proms.bat
@@ -0,0 +1,9 @@
+make_vhdl_prom 6530sy80.bin RRIOT_6530_sys80_prom.vhd
+
+REM below rom is for pink panther (664) - substitute rom to suit your gottlieb sys80 pinball
+make_vhdl_prom 664.snd snd_prom.vhd
+
+pause
+
+
+
